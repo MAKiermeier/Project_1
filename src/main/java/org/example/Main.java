e94805315972027aa1d8d257e2c1522af805c391
Martin Kiermeier <martin.kiermeier@st.hs-regesburg.de>
2026-04-15 17:54:49 +0200

    Java FX test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.example.view.main_menu.Main_menu;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Main_menu main_menu = new Main_menu();
+        main_menu.main_menu_launch();
     }
 }
---
+++ src/main/java/org/example/view/main_menu/Main_menu.java
@@ -0,0 +1,25 @@
+package org.example.view.main_menu;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+public class Main_menu extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Hallo JavaFX!");
+
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 400, 300);
+
+        stage.setTitle("Mein erstes JavaFX Fenster");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void main_menu_launch(){
+    launch();
+    }
+}
+
