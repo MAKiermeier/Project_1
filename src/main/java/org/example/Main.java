d2102a4b95318c0cf3600520c76e48150bc0829c
Martin Kiermeier <martin.kiermeier@st.hs-regesburg.de>
2026-04-26 19:45:47 +0200

    deck_class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,6 +12,7 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) {
         Main_menu main_menu = new Main_menu();
+        main_menu.main_menu_launch();
         try{
         Connection connection = connect();
         read_decks(connection);
--- src/main/java/org/example/cards_decks/deck.java
+++ src/main/java/org/example/cards_decks/deck.java
@@ -1,4 +1,7 @@
 package org.example.cards_decks;
 
 public class deck {
+    int    deck_class_id;
+    String deck_class_name;
+    String deck_class_created_at; //change datatype
 }
